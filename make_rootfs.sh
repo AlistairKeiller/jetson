a801423ba8097ad7e617b9a35a8f911d3f869cc4
Alistair Keiller <alistair@keiller.net>
2022-06-06 20:03:16 -0700

    remove other files

--- make_rootfs.sh
+++ make_rootfs.sh
@@ -1,5 +1,12 @@
 echo "Seting variables"
-bash set_variables.sh
+RELEASE=bionic
+JETSON_NAME=jetski
+JETSON_USR=alistair
+JETSON_PWD=password
+JETSON_BOARD=jetson-nano
+JETSON_BOARD_REV=300
+BSP_URL=https://developer.nvidia.com/embedded/l4t/r32_release_v6.1/t210/jetson-210_linux_r32.6.1_aarch64.tbz2
+WORK_DIR=/home/runner/work/jetson/jetson
 
 
 echo "Installing packages on host system"
@@ -65,3 +72,43 @@ apt install -y --no-install-recommends \
 echo "Cleaning up"
 apt autoremove -y
 apt clean
+
+
+echo "Removing files that conflict with LT4" # might not be nessesary
+rm ${WORK_DIR}/rootfs/dev/random ${WORK_DIR}/rootfs/dev/urandom
+
+
+echo "Downloading BSP"
+wget -O ${WORK_DIR}/JETSON_BSP.tbz2 ${BSP_URL}
+
+
+echo "Extracting BSP"
+tar -jpxf ${WORK_DIR}/JETSON_BSP.tbz2 -C ${WORK_DIR}
+
+
+echo "Removing tar"
+rm ${WORK_DIR}/JETSON_BSP.tbz2
+
+
+echo "Moving rootfs to BSP"
+rm -r ${WORK_DIR}/Linux_for_Tegra/rootfs
+mv ${WORK_DIR}/rootfs ${WORK_DIR}/Linux_for_Tegra
+
+
+echo "Applying jetson binaries"
+cd ${WORK_DIR}/Linux_for_Tegra/
+./apply_binaries.sh
+
+
+echo "Adding ${JETSON_USR} as user"
+cd ${WORK_DIR}/Linux_for_Tegra/tools
+./l4t_create_default_user.sh -u ${JETSON_USR} -p ${JETSON_PWD}
+
+
+echo "Making ${JETSON_USR} a sudoer"
+echo "${JETSON_USR} ALL=(ALL) NOPASSWD: ALL" | tee ${WORK_DIR}/Linux_for_Tegra/rootfs/etc/sudoers.d/${JETSON_USR}
+
+
+echo "creating image"
+cd ${WORK_DIR}/Linux_for_Tegra/tools
+./jetson-disk-image-creator.sh -o ${WORK_DIR}/JETSON.img -b ${JETSON_BOARD} -r ${JETSON_BOARD_REV}
--- set_variables.sh
+++
@@ -1,8 +0,0 @@
-RELEASE=bionic
-JETSON_NAME=jetski
-JETSON_USR=alistair
-JETSON_PWD=password
-JETSON_BOARD=jetson-nano
-JETSON_BOARD_REV=300
-BSP_URL=https://developer.nvidia.com/embedded/l4t/r32_release_v6.1/t210/jetson-210_linux_r32.6.1_aarch64.tbz2
-WORK_DIR=/home/runner/work/jetson/jetson
