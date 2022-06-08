e73d4442efaca2632ab0d277ebdfaf9ebfc8e474
Alistair Keiller <alistair@keiller.net>
2022-06-08 12:26:29 -0700

    remove package version requirement

--- create_image.sh
+++ create_image.sh
@@ -47,8 +47,8 @@ deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports ${RELEASE}-backports main 
 
 
 echo "Installing packages"
-schroot -c jetson-image -- apt-get update
-echo apt-get -y --no-install-recommends --allow-downgrades install $(xargs -a tools/samplefs/nvubuntu-bionic-aarch64-packages) | schroot -c jetson-image
+schroot -c jetson-image -- apt-get updates
+echo apt-get -y --no-install-recommends --allow-downgrades install $(cut -f 1 -d "=" tools/samplefs/nvubuntu-bionic-aarch64-packages | xargs) | schroot -c jetson-image
 scroot -c jetson-image -- sync
 schroot -c jetson-image -- apt-get clean
 schroot -c jetson-image -- sync
