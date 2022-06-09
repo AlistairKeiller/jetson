cc5de9b13b94a8fbc722a92a3ae262aec06a2d31
Alistair Keiller <alistair@keiller.net>
2022-06-08 21:52:31 -0700

    test

--- create_image.sh
+++ create_image.sh
@@ -6,10 +6,11 @@ JETSON_USR=alistair
 JETSON_PWD=password
 JETSON_BOARD=jetson-nano
 JETSON_BOARD_REV=300
-BSP_URL=https://developer.nvidia.com/embedded/l4t/r34_release_v1.1/release/jetson_linux_r34.1.1_aarch64.tbz2
+BSP_URL=https://developer.nvidia.com/embedded/l4t/r32_release_v7.2/t210/jetson-210_linux_r32.7.2_aarch64.tbz2
 
 echo "Installing dependencies"
-sudo apt-get -y install debootstrap qemu-user-static binfmt-support libxml2-utils
+apt-get update
+apt-get -y install debootstrap qemu-user-static binfmt-support libxml2-utils
 
 
 echo "Downloading BSP"
@@ -23,63 +24,62 @@ cd Linux_for_Tegra/rootfs
 rm README.txt
 
 
-echo "Starting debootstrap"
-debootstrap --variant=minbase --arch=arm64 --foreign ${RELEASE} .
+echo "Running debootstrap"
+debootstrap --arch=arm64 --foreign --variant=minbase ${RELEASE} .
 cp /usr/bin/qemu-aarch64-static usr/bin/
-
-
-echo "Mounting rootfs"
-mount /sys ./sys -o bind
-mount /proc ./proc -o bind
-mount /dev ./dev -o bind
-mount /dev/pts ./dev/pts -o bind
-
-
-echo "Finishing debootstrap"
+echo "nameserver 1.1.1.1" | tee etc/resolv.conf
 chroot . /debootstrap/debootstrap --second-stage
 
 
-echo "Setting repos"
-echo "deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports ${RELEASE} main restricted universe multiverse
-deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports ${RELEASE}-updates main restricted universe multiverse
-deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports ${RELEASE}-security main restricted universe multiverse
-deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports ${RELEASE}-backports main restricted universe multiverse" | tee etc/apt/sources.list
+echo "Mounting rootfs"
+mount --bind /sys ./sys -o
+mount --bind /proc ./proc -o
+mount --bind /dev ./dev -o
+mount --bind /dev/pts ./dev/pts -o
 
 
-echo "Installing packages"
+echo "test"
 chroot . apt-get update
-echo apt-get -y install $(cut -d"=" -f1  ../tools/samplefs/nvubuntu-focal-minimal-aarch64-packages | xargs) | DEBIAN_FRONTEND=noninteractive chroot .
-chroot . sync
-chroot . apt-get clean
-chroot . sync
 
 
-echo "Unmounting rootfs"
-umount ./sys
-umount ./proc
-umount ./dev/pts
-umount ./dev
+# echo "Setting repos"
+# echo "deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports ${RELEASE} main restricted universe multiverse
+# deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports ${RELEASE}-updates main restricted universe multiverse
+# deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports ${RELEASE}-security main restricted universe multiverse
+# deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports ${RELEASE}-backports main restricted universe multiverse" | tee etc/apt/sources.list
 
 
-echo "Removing conflicting and unnecessary files"
-rm usr/bin/qemu-aarch64-static
-rm -rf var/lib/apt/lists/*
-rm -rf dev/*
-rm -rf var/log/*
-rm -rf var/cache/apt/archives/*.deb
-rm -rf var/tmp/*
-rm -rf tmp/*
+# echo "Installing packages"
+# chroot . apt-get update
+# chroot . apt-get install -y \
+#     libgles2 libpangoft2-1.0-0 libxkbcommon0 libwayland-egl1 libwayland-cursor0 libunwind8 libasound2 libpixman-1-0 libjpeg-turbo8 libinput10 libcairo2 device-tree-compiler iso-codes libffi6 libncursesw5 libdrm-common libdrm2 libegl-mesa0 libegl1 libegl1-mesa libgtk-3-0 python2 python-is-python2 libgstreamer1.0-0 libgstreamer-plugins-bad1.0-0 \
+#     bash-completion build-essential btrfs-progs cmake curl dnsutils htop iotop isc-dhcp-client iputils-ping kmod linux-firmware locales net-tools netplan.io pciutils python3-dev ssh sudo udev unzip usbutils neovim wpasupplicant
+# chroot . sync
+# chroot . apt-get clean
+# chroot . sync
 
 
-echo "Applying binary patches"
-cd ..
-./apply_binaries.sh
+# echo "Unmounting rootfs"
+# umount ./sys
+# umount ./proc
+# umount ./dev/pts
+# umount ./dev
 
 
-echo "Adding ${JETSON_USR} as user"
-cd tools
-./l4t_create_default_user.sh -u ${JETSON_USR} -p ${JETSON_PWD} -n ${JETSON_NAME} --autologin --accept-license
+# echo "Removing conflicting and unnecessary files"
+# rm usr/bin/qemu-aarch64-static
+# rm -rf var/lib/apt/lists/*
+# rm -rf dev/*
+# rm -rf var/log/*
+# rm -rf var/cache/apt/archives/*.deb
+# rm -rf var/tmp/*
+# rm -rf tmp/*
 
 
-echo "Creating image"
-./jetson-disk-image-creator.sh -o ../../jetson_image.img -b jetson-nano -r 300
+# echo "Applying binary patches"
+# cd ..
+# ./apply_binaries.sh
+
+
+# echo "Creating image"
+# ./jetson-disk-image-creator.sh -o ../../jetson_image.img -b jetson-nano -r 300
