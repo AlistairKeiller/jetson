b35e24262faa7e7f5c67df4b57e63628b5f298b4
Alistair Keiller <alistair@keiller.net>
2022-06-08 22:39:20 -0700

    test

--- create_image.sh
+++ create_image.sh
@@ -15,7 +15,7 @@ apt-get -y install debootstrap qemu-user-static binfmt-support libxml2-utils
 
 echo "Downloading and Extracting BSP"
 wget -O- ${BSP_URL} | tar -xjp
-rm *.nc.gz
+# rm *.nc.gz
 cd Linux_for_Tegra/rootfs
 rm README.txt
 
@@ -34,49 +34,62 @@ mount --bind /dev ./dev
 mount --bind /dev/pts ./dev/pts
 
 
-echo "test"
+echo "Setting repos"
+echo "deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports ${RELEASE} main restricted universe multiverse
+deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports ${RELEASE}-updates main restricted universe multiverse
+deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports ${RELEASE}-security main restricted universe multiverse
+deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports ${RELEASE}-backports main restricted universe multiverse
+deb http://ports.ubuntu.com/ubuntu-ports/ bionic main" | tee etc/apt/sources.list
+
+
+echo "Installing packages"
 chroot . apt-get update
+chroot . apt-get -y install \
+    libgles2 libpangoft2-1.0-0 libxkbcommon0 libwayland-egl1 libwayland-cursor0 libunwind8 libasound2 libpixman-1-0 libjpeg-turbo8 libinput10 libcairo2 device-tree-compiler iso-codes libffi6 libncursesw5 libdrm-common libdrm2 libegl-mesa0 libegl1 libegl1-mesa libgtk-3-0 python2 python-is-python2 libgstreamer1.0-0 libgstreamer-plugins-bad1.0-0 \
+    bash-completion build-essential btrfs-progs cmake curl dnsutils htop iotop isc-dhcp-client iputils-ping kmod linux-firmware locales net-tools netplan.io pciutils python3-dev ssh sudo udev unzip usbutils neovim wpasupplicant
 
 
-# echo "Setting repos"
-# echo "deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports ${RELEASE} main restricted universe multiverse
-# deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports ${RELEASE}-updates main restricted universe multiverse
-# deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports ${RELEASE}-security main restricted universe multiverse
-# deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports ${RELEASE}-backports main restricted universe multiverse
-# deb http://ports.ubuntu.com/ubuntu-ports/ bionic main" | tee etc/apt/sources.list
+echo "Generating locales"
+chroot . locale_gen en_US.UTF-8
 
 
-# echo "Installing packages"
-# chroot . apt-get update
-# chroot . apt-get -y install \
-#     libgles2 libpangoft2-1.0-0 libxkbcommon0 libwayland-egl1 libwayland-cursor0 libunwind8 libasound2 libpixman-1-0 libjpeg-turbo8 libinput10 libcairo2 device-tree-compiler iso-codes libffi6 libncursesw5 libdrm-common libdrm2 libegl-mesa0 libegl1 libegl1-mesa libgtk-3-0 python2 python-is-python2 libgstreamer1.0-0 libgstreamer-plugins-bad1.0-0 \
-#     bash-completion build-essential btrfs-progs cmake curl dnsutils htop iotop isc-dhcp-client iputils-ping kmod linux-firmware locales net-tools netplan.io pciutils python3-dev ssh sudo udev unzip usbutils neovim wpasupplicant
-# chroot . sync
-# chroot . apt-get clean
-# chroot . sync
+echo "Unmounting rootfs"
+chroot . sync
+chroot . apt-get clean
+chroot . sync
+umount ./sys
+umount ./proc
+umount ./dev/pts
+umount ./dev
 
 
-# echo "Unmounting rootfs"
-# umount ./sys
-# umount ./proc
-# umount ./dev/pts
-# umount ./dev
+echo "Configuring netplan"
+echo "network:
+  version: 2
+  renderer: networkd
+  ethernets:
+    eth0:
+      dhcp4: true" | tee etc/netplan/config.yaml
 
 
-# echo "Removing conflicting and unnecessary files"
-# rm usr/bin/qemu-aarch64-static
-# rm -rf var/lib/apt/lists/*
-# rm -rf dev/*
-# rm -rf var/log/*
-# rm -rf var/cache/apt/archives/*.deb
-# rm -rf var/tmp/*
-# rm -rf tmp/*
+echo "Removing conflicting and unnecessary files"
+rm usr/bin/qemu-aarch64-static
+rm -rf var/lib/apt/lists/*
+rm -rf dev/*
+rm -rf var/log/*
+rm -rf var/cache/apt/archives/*.deb
+rm -rf var/tmp/*
+rm -rf tmp/*
 
 
-# echo "Applying binary patches"
-# cd ..
-# ./apply_binaries.sh
+echo "Applying binary patches"
+cd ..
+./apply_binaries.sh
+
+echo "Adding ${JETSON_USR} as user"
+cd tools
+./l4t_create_default_user.sh -u ${JETSON_USR} -p ${JETSON_PWD} -n ${JETSON_NAME} --autologin --accept-license
 
 
-# echo "Creating image"
-# ./jetson-disk-image-creator.sh -o ../../jetson_image.img -b jetson-nano -r 300
+echo "Creating image"
+./jetson-disk-image-creator.sh -o ../../jetson_image.img -b jetson-nano -r 300
