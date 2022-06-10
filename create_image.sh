a7bc1184dfc94f72b99c322b9b39fff697f722c4
Alistair Keiller <alistair@keiller.net>
2022-06-10 09:30:17 -0700

    test

--- create_image.sh
+++ create_image.sh
@@ -8,9 +8,9 @@ JETSON_BOARD=jetson-nano
 JETSON_BOARD_REV=300
 BSP_URL=https://developer.nvidia.com/embedded/l4t/r32_release_v7.2/t210/jetson-210_linux_r32.7.2_aarch64.tbz2
 ADDITIONAL_PACKAGES=
-SSH=true
-WIFI=true
-LOCALES=true
+SSH=false
+WIFI=false
+LOCALES=false
 
 
 echo "Installing dependencies on host"
