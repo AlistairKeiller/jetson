9d91899b98785b45e138426b0e347e5ed36c8485
Alistair Keiller <alistair@keiller.net>
2022-06-11 17:26:21 -0700

    adding xubuntu-core

--- create_image.sh
+++ create_image.sh
@@ -50,6 +50,7 @@ chroot . apt-get -y --no-install-recommends install \
     bash-completion build-essential btrfs-progs cmake curl dnsutils htop iotop isc-dhcp-client iputils-ping kmod linux-firmware locales net-tools netplan.io pciutils python3-dev ssh sudo udev unzip usbutils neovim wpasupplicant \
     gdisk parted `# for nvresizefs.sh` \
     ca-certificates `# to allow using apt`\
+    onboard xorg xubuntu-core \
     ${ADDITIONAL_PACKAGES}
 
 
@@ -66,6 +67,7 @@ echo "network:
       dhcp4: true" | tee etc/netplan/config.yaml
 
 
+echo "Adding nvresizefs service"
 echo "[Unit]
 Description=Resize SD Card root partition and filesystem
 Before=nvfb.service
@@ -83,7 +85,7 @@ WantedBy=multi-user.target" | tee etc/systemd/system/nvresizefs.service
 echo "Enabling services"
 chroot . systemctl enable nvresizefs.service
 chroot . systemctl enable systemd-networkd
-chroot . systemctl enable ssh
+chroot . systemctl enable ssh 
 
 
 echo "Unmounting rootfs"
