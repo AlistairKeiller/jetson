c9c4d4ad8b0b2ab37de36e71eb31d7a77f689e0e
Alistair Keiller <alistair@keiller.net>
2022-06-13 10:10:04 -0700

    remove ca-certificates

--- create_image.sh
+++ create_image.sh
@@ -58,7 +58,7 @@ chroot . apt-get update
 chroot . apt-get -y --no-install-recommends install \
     libgles2 libpangoft2-1.0-0 libxkbcommon0 libwayland-egl1 libwayland-cursor0 libunwind8 libasound2 libpixman-1-0 libjpeg-turbo8 libinput10 libcairo2 device-tree-compiler iso-codes libffi6 libncursesw5 libdrm-common libdrm2 libegl-mesa0 libegl1 libegl1-mesa libgtk-3-0 python2 python-is-python2 libgstreamer1.0-0 libgstreamer-plugins-bad1.0-0 python3 \
     bash-completion build-essential btrfs-progs cmake curl dnsutils htop iotop isc-dhcp-client iputils-ping kmod linux-firmware locales net-tools netplan.io pciutils python3-dev ssh sudo udev unzip usbutils neovim wpasupplicant \
-    ca-certificates `# to allow using apt` \
+    # ca-certificates `# to allow using apt` \
     ${ADDITIONAL_PACKAGES}
 
 
