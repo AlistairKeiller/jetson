8d0136f64f0a0b0f3d46bde4b1a0ccf121845405
Alistair Keiller <alistair@keiller.net>
2022-06-13 09:41:12 -0700

    remove sudo

--- create_image.sh
+++ create_image.sh
@@ -55,15 +55,17 @@ fi
 if( ${CUDA} == true ) ; then
   chroot . apt-get update
   chroot . apt-get -y --no-install-recommends install software-properties-common wget dirmngr gpg-agent
-  chroot . sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/cross-linux-sbsa/3bf863cc.pub
-  chroot . sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/cross-linux-sbsa/ /"
+  chroot . apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/cross-linux-sbsa/3bf863cc.pub
+  chroot . add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/cross-linux-sbsa/ /"
   ADDITIONAL_PACKAGES+=" cuda-cross-sbsa"
+fi
+if( ${AUTOMATIC_RESIZE_PARTITION} == true ) ; then
+  ADDITIONAL_PACKAGES+=" gdisk parted"
 fi
 chroot . apt-get update
 chroot . apt-get -y --no-install-recommends install \
     libgles2 libpangoft2-1.0-0 libxkbcommon0 libwayland-egl1 libwayland-cursor0 libunwind8 libasound2 libpixman-1-0 libjpeg-turbo8 libinput10 libcairo2 device-tree-compiler iso-codes libffi6 libncursesw5 libdrm-common libdrm2 libegl-mesa0 libegl1 libegl1-mesa libgtk-3-0 python2 python-is-python2 libgstreamer1.0-0 libgstreamer-plugins-bad1.0-0 python3 \
     bash-completion build-essential btrfs-progs cmake curl dnsutils htop iotop isc-dhcp-client iputils-ping kmod linux-firmware locales net-tools netplan.io pciutils python3-dev ssh sudo udev unzip usbutils neovim wpasupplicant \
-    gdisk parted `# for nvresizefs.sh` \
     ca-certificates `# to allow using apt` \
     ${ADDITIONAL_PACKAGES}
 
